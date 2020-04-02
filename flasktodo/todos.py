3b11ba81b6ca6fbc4d2e31388021224b1c5f19ab
Andrew Scholly <SATmaster728@gmail.com>
2020-04-02 15:54:29 -0400

    Completed update, delete, and mark completed

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -19,6 +19,10 @@ def index():
         completed = request.form.get('completed')
         uncompleted = request.form.get('uncompleted')
         all = request.form.get('all')
+        remove = request.form.get('remove')
+        make_completed = request.form.get('make_completed')
+        update_text = request.form.get('update_text')
+        update_which = request.form.get('update_which')
         if description != None:
             #Add a new task
             cur.execute(
@@ -34,6 +38,29 @@ def index():
 
             else:
                 cur.execute('SELECT * FROM todos WHERE completed = TRUE')
+        #Remove, update, or mark a task as completed
+        if make_completed != None or remove != None or update_text != None or update_which != None:
+
+            if make_completed != None:
+                cur.execute(
+                "UPDATE todos SET completed = 'true' where id = %s",
+                (make_completed,)
+                )
+                cur.execute('SELECT * FROM todos')
+
+            elif remove != None:
+                cur.execute(
+                'DELETE FROM todos WHERE id = %s',
+                (remove,)
+                )
+                cur.execute('SELECT * FROM todos')
+
+            else:
+                cur.execute(
+                "Update todos SET description = %s where id = %s",
+                (update_text, update_which)
+                )
+                cur.execute('SELECT * FROM todos')
 
 
     conn.commit()
@@ -44,6 +71,3 @@ def index():
 
 
     return render_template("index.html", todos=todos)
-
-[1, 2]
-(1,)
